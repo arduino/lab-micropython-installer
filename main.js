c419c5ff9e9eb97c6e0b57a1af2e3e9dfc62bae3
Sebastian Romero <s.romero@arduino.cc>
2023-04-07 07:45:14 +0200

    Close app when all windows are closed

--- main.js
+++ main.js
@@ -31,9 +31,7 @@ app.whenReady().then(async () => {
 })
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit()
-    }
+    app.quit()
 })
 
 app.on('activate', () => {
