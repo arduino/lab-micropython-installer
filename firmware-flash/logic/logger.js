856ee76e91e26a062604b3993a93545ad773f266
Sebastian Romero <s.romero@arduino.cc>
2023-04-20 11:20:12 +0200

    Add log levels

--- firmware-flash/logic/logger.js
+++ firmware-flash/logic/logger.js
@@ -1,9 +1,26 @@
 class Logger {
-    constructor(onLog, printToConsole = false) {
+    // Enum for log levels
+    static get LOG_LEVEL() {
+        return {
+            DEBUG: 0,
+            INFO: 1,
+            WARNING: 2,
+            ERROR: 3,
+            NONE: 4
+        };
+    }
+
+    constructor(onLog, printToConsole = false, logLevel = Logger.LOG_LEVEL.INFO) {
         this.onLog = onLog;
         this.printToConsole = printToConsole;
+        this.logLevel = logLevel;
     }
-    log(message) {
+
+    log(message, level = Logger.LOG_LEVEL.INFO) {
+        if(level < this.logLevel) {
+            return;
+        }
+
         if(this.onLog) {
             this.onLog(message);
         }
