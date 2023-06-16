e15c091acec71ccfe1474653c92921db31fcb73c
Sebastian Romero <s.romero@arduino.cc>
2023-06-16 14:29:55 +0200

    Remove unused binary leftovers

--- forge.config.js
+++ forge.config.js
@@ -25,16 +25,24 @@ module.exports = {
     prune: true,
     derefSymlinks: true,
     afterCopy: [(buildPath, electronVersion, platform, arch, callback) => {
+      const fs = require('fs');
+      const path = require('path');
       // Remove files under node_modules/@serialport/bindings-cpp/build/node_gyp_bins/
       // because the cause notarization issues and they are not needed after building.
       // One of the files is a symlink to python which is outside of the app bundle.
       // SEE: https://github.com/nodejs/node-gyp/issues/2713#issuecomment-1400959609
-      const fs = require('fs');
-      const path = require('path');
-      const dir = path.join(buildPath, 'node_modules/firmware-flash/node_modules/@serialport/bindings-cpp/build/node_gyp_bins/');
-      if (fs.existsSync(dir)) {
-        fs.rmdirSync(dir, { recursive: true });
-      }
+      const nodeGypBinsDir = path.join(buildPath, 'node_modules/firmware-flash/node_modules/@serialport/bindings-cpp/build/node_gyp_bins/');
+      // Remove files under node_modules/@serialport/bindings-cpp/prebuilds/
+      // because they are not needed after building and they cause code signing issues under Windows.
+      // signtool.exe would e.g. try to sign android-arm\node.napi.armv7.node which will in fail.
+      const nodeGypPrebuildsDir = path.join(buildPath, 'node_modules/firmware-flash/node_modules/@serialport/bindings-cpp/prebuilds/');
+      
+      [nodeGypBinsDir, nodeGypPrebuildsDir].forEach(dir => {
+        if (fs.existsSync(dir)) {
+          fs.rmdirSync(dir, { recursive: true });
+        }
+      });
+
       callback();
     }],
     osxSign: {
