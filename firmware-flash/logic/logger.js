13617921c3acbec2805faf41b0127860840cdb6f
Sebastian Romero <s.romero@arduino.cc>
2023-08-03 16:17:54 +0200

    Add default logger

--- firmware-flash/logic/logger.js
+++ firmware-flash/logic/logger.js
@@ -1,4 +1,14 @@
+let defaultLogger;
+
 class Logger {
+
+    static get defaultLogger() {
+        if(!defaultLogger) {
+            defaultLogger = new Logger();
+        }
+        return defaultLogger;
+    }
+
     // Enum for log levels
     static get LOG_LEVEL() {
         return {
@@ -10,19 +20,28 @@ class Logger {
         };
     }
 
-    constructor(onLog, printToConsole = false, logLevel = Logger.LOG_LEVEL.INFO) {
-        this.onLog = onLog;
+    constructor(onLog = null, printToConsole = true, logLevel = Logger.LOG_LEVEL.INFO) {
+        this.onLogCallback = onLog;
         this.printToConsole = printToConsole;
         this.logLevel = logLevel;
     }
+
+    set onLog(onLogCallback) {
+        this.onLogCallback = onLogCallback;
+    }
+
+    setLogLevel(logLevel) {
+        this.logLevel = logLevel;
+    }
+
 
     log(message, level = Logger.LOG_LEVEL.INFO) {
         if(level < this.logLevel) {
             return;
         }
 
-        if(this.onLog) {
-            this.onLog(message, level);
+        if(this.onLogCallback) {
+            this.onLogCallback(message, level);
         }
 
         if(this.printToConsole) {
